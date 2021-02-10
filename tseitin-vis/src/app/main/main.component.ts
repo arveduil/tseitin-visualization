b1b78a2d24bfb087a9d474dbc04ca65a1f78039b
pboszczyk <pboszczyk@gmail.com>
2021-02-11 00:27:55 +0100

    Added tseitin variables

--- tseitin-vis/src/app/main/main.component.ts
+++ tseitin-vis/src/app/main/main.component.ts
@@ -17,10 +17,7 @@ export class MainComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.service.get().subscribe(res => {
-      this.response = res;
-      this.createGraph();
-    });
+    this.transform();
   }
 
   transform() {
@@ -28,6 +25,55 @@ export class MainComponent implements OnInit {
       this.response = res;
       this.createGraph();
     });
+  }
+
+  private getTseitinVariables(formula: string, clauses: Map<number, Array<number>>, terms: number): [Set<number>, Map<number, string>]  {
+    const formulaWithoutAnd = formula
+      .replace(/ and /g, '');
+
+    let res = formulaWithoutAnd
+        .split(')')
+        .map(alternative => alternative
+          .split(' ')
+          .filter(variable => variable !== 'or')
+          .map(variable => variable
+            .replace('(', '')
+            .replace('!', ''))
+        )
+      //  .splice(-1, 1)
+    ;
+
+    res.splice(-1, 1);
+
+    let output = new Set<number>();
+    let primaryVariables = new Map<number,string>();
+
+    console.log(res);
+    console.log(clauses);
+    if (res.length !== clauses.size) {
+      console.log('WRONG AMOUNT OF CLAUSES ' + res.length + ' ' + clauses.size);
+    }
+    for (let i = terms + 1; i <= res.length + terms; i++) {
+      const alternativesFromFormula = res[i - terms - 1];
+      const clause = clauses.get(i);
+
+      if (alternativesFromFormula.length !== clause.length) {
+        console.log('alternativesFromFormula: ' + alternativesFromFormula);
+        console.log('clause: ' + clause);
+      }
+
+      for (let j = 0; j < clause.length; j++) {
+        console.log(alternativesFromFormula[j]);
+        if (alternativesFromFormula[j].includes('phi')) {
+          output.add(Math.abs(clause[j]));
+          //console.log(clause[j]);
+        }else{
+          primaryVariables.set(Math.abs(clause[j]) , alternativesFromFormula[j]);
+        }
+      }
+    }
+    console.log(output);
+    return [output, primaryVariables];
   }
 
   private createGraph() {
@@ -38,23 +84,20 @@ export class MainComponent implements OnInit {
       .map(clause => clause.split(' ').filter(val => val !== '').map(str => +str))
       .filter(clauseSet => clauseSet.length > 0);
 
-    console.log(dimacs);
-
-
     let clauseToExpress = new Map();
     let variableOccurenceEdges = [];
 
-    for (let i = 0; i < dimacs.length; i++) {
 
+    for (let i = 0; i < dimacs.length; i++) {
 
 
       let clauseId = terms + i + 1;
       clauseToExpress.set(clauseId, dimacs[i]);
       dimacs[i].forEach(expr => {
-        let border = 2;
-        // if (Math.abs( expr) >= this.response.originalTermsCount) {
-        //   border = 5;
-        // }
+          let border = 2;
+          // if (Math.abs( expr) >= this.response.originalTermsCount) {
+          //   border = 5;
+          // }
 
           if (expr > 0) {
             variableOccurenceEdges.push({from: clauseId, to: expr, width: border});
@@ -65,18 +108,34 @@ export class MainComponent implements OnInit {
       );
     }
 
-
+    console.log('clause to express');
     console.log(clauseToExpress);
+    const [tseitinNodesIds, primaryVariables] = this.getTseitinVariables(this.response.tseitinFormula, clauseToExpress, terms);
+    console.log("prim")
+    console.log(primaryVariables);
+    // for (let varOcc in variableOccurenceEdges){
+    //   if(Math.abs(varOcc['from'])){
+    //
+    //   }
+    // }
 
     let rawNodes = [];
     for (let i = 1; i <= terms; i++) {
-      let shape = 'dot';
-      // if (i <= this.response.originalTermsCount) {
-      //   shape = 'dot';
-      // } else {
-      //   shape = 'hexagon';
-      // }
-      rawNodes.push({id: i, label: '' + i, shape: shape, y: -200, x: i * 150 + 100, color: 'blue'});
+      let shapeValue = 'dot';
+      let nodeSize = 30;
+      let colorName = 'blue';
+      let nodeLabel = '' + i;
+
+      if (tseitinNodesIds.has(i)) {
+        shapeValue = 'hexagon';
+        nodeSize = 50;
+        colorName = 'green';
+      } else{
+        console.log( primaryVariables[i])
+        nodeLabel =    primaryVariables.get(i) + '=' + nodeLabel;
+      }
+
+      rawNodes.push({id: i, label: nodeLabel, shape: shapeValue, y: -200, x: i * 150 + 100, color: colorName, size: nodeSize});
     }
 
     for (let i = terms + 1; i <= terms + clauses; i++) {
@@ -101,7 +160,6 @@ export class MainComponent implements OnInit {
     };
     const options = {
       nodes: {
-        size: 30,
         font: {
           size: 32,
           color: 'black'
@@ -116,4 +174,6 @@ export class MainComponent implements OnInit {
     };
     const network = new Network(container, data, options);
   }
+
+
 }
